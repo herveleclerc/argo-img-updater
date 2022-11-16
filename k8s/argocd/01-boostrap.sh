360a323f3e6d42c2ba080e191509d2c66106767d
herveleclerc <herve.leclerc@gmail.com>
2022-11-16 13:34:02 +0100

    apps

--- k8s/argocd/01-boostrap.sh
+++ k8s/argocd/01-boostrap.sh
@@ -7,3 +7,8 @@ kubectx kind-kind
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+kubectl patch deployments.apps argocd-repo-server --patch-file vault-plugin/01-patch-argo.yaml
+kubectl patch configmap argocd-cm -n argocd --patch-file vault-plugin/02-argo-cm.yaml
